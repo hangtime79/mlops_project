0e8e52de374004772ff225ce7094b5a1fa4924f7
gcase <gcase>
2019-12-21 15:07:11 -0800

    Recipe HOTELCANCELLATIONMODELMONITORING.compute_Hotel_1 saved through notebook notebook editor for compute_Hotel_1

--- recipes/compute_Hotel_1.r
+++ recipes/compute_Hotel_1.r
@@ -1,12 +1,20 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 library(dataiku)
+library(caret)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe inputs
-hotel_Cancellation_filtered <- dkuReadDataset("Hotel_Cancellation_filtered", samplingMethod="full", nbRows=100000)
+data <- dkuReadDataset("Hotel_Cancellation_filtered", samplingMethod="full", columns = c("Is_Canceled"))
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a R dataframe or data table
-hotel_1 <- hotel_Cancellation_filtered # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+split_percentage <- .75
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+split <- createDataPartition(data$Is_Canceled, p=split_percentage, list=FALSE)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+str(split)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe outputs
 dkuWriteDataset(hotel_1,"Hotel_1")
