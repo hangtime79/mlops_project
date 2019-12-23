10466d4a91bbb036e1ae4e276bffb3e8b294259d
gcase <gcase>
2019-12-23 03:17:43 -0800

    Saved recipe: HOTELCANCELLATIONMODELMONITORING.compute_Hotel_1

--- recipes/compute_Hotel_1.r
+++ recipes/compute_Hotel_1.r
@@ -4,8 +4,18 @@ library(caret)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 target_col <- "Is_Canceled"
+target_col
 split_percentage <- .75
+
+
 input_data <- "Hotel_Cancellation_Prepared"
+
+output_table_prefix <- "Hotel_1"
+output_train_suffix <- "Train"
+output_test_suffix <- "Test"
+
+output_train <- paste(output_table_prefix, "_", output_train_suffix, sep = "")
+output_test <- paste(output_table_prefix, "_", output_test_suffix, sep = "")
 seed_val <- 456789
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
