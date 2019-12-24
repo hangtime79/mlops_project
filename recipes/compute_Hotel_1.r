e619e093c99427eceda63bcd52fc5bab791c8585
gcase <gcase>
2019-12-24 01:53:19 -0800

    Saved recipe: HOTELCANCELLATIONMODELMONITORING.compute_Hotel_1

--- recipes/compute_Hotel_1.r
+++ recipes/compute_Hotel_1.r
@@ -3,23 +3,33 @@ library(dataiku)
 library(caret)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-target_col <- "Is_Canceled"
-target_col_is_factor <- TRUE
-split_percentage <- .75
-seed_val <- 456789
-input_data <- "Hotel_1"
+form_target_col <- "Is_Canceled"
+form_target_col_is_factor <- TRUE
+form_split_percentage <- .75
+form_seed_val <- 456789
+form_df <-"Hotel_1"
+form_output_table_prefix <- "Hotel_1"
+form_output_train_suffix <- "Train"
+form_output_test_suffix <- "Test"
+
+output_table_prefix <- form_output_table_prefix
+output_train_suffix <- form_output_train_suffix
+output_test_suffix <- form_output_test_suffix
+target_col <- form_target_col
+target_col_is_factor <- form_target_col_is_factor
+split_percentage <- form_split_percentage
+seed_val <- form_seed_val
+df <- form_df
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-output_table_prefix <- "Hotel_1"
-output_train_suffix <- "Train"
-output_test_suffix <- "Test"
+
 
 output_train <- paste(output_table_prefix, "_", output_train_suffix, sep = "")
 output_test <- paste(output_table_prefix, "_", output_test_suffix, sep = "")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe inputs
-data <- dkuReadDataset(input_data, samplingMethod="full")
+data <- dkuReadDataset(df, samplingMethod="full")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 if (target_col_is_factor) {
